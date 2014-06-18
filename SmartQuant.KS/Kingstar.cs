860c82100a49491020e9bb93a37953433c35068d
javagg <lu.lee05@gmail.com>
2014-06-19 00:57:22 +0800

    nothing to say

--- SmartQuant.KS/Kingstar.cs
+++ SmartQuant.KS/Kingstar.cs
@@ -2,11 +2,36 @@
 
 namespace SmartQuant.KS
 {
-	public class Kingstar : Provider, IExecutionProvider, IHistoricalDataProvider, IInstrumentProvider, IDataProvider
-	{
-		public Kingstar ()
-		{
-		}
-	}
+    public class Kingstar : Provider, IExecutionProvider, IHistoricalDataProvider, IInstrumentProvider, IDataProvider
+    {
+        public Kingstar(Framework framework)
+            : base(framework)
+        {
+        }
+
+        void IHistoricalDataProvider.Cancel(string requestId)
+        {
+        }
+
+        void IInstrumentProvider.Cancel(string requestId)
+        {
+        }
+
+        public override void Subscribe(Instrument instrument)
+        {
+        }
+
+        public override void Unsubscribe(Instrument instrument)
+        {
+        }
+
+        public override void Connect()
+        {
+        }
+
+        public override void Disconnect()
+        {
+        }
+    }
 }
 
